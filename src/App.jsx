aa5b5fd53b97ee903b25c9098aa193e47f309498
DesideApp <deside.w3app@gmail.com>
2024-12-07 00:32:28 +0100

    api

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,6 @@ import Header from "./components/Header";
 import Home from "./pages/Home";
 import Chat from './pages/chat/Chat.jsx';
 import BottomBar from "./components/BottomBar.jsx";
-import cors from 'cors';
-
-const allowedOrigins = ['https://deside-app.vercel.app']; // Añade aquí tu dominio de producción
 
 function App() {
     return (
@@ -29,14 +26,3 @@ function App() {
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
 
-app.use(cors({
-    origin: (origin, callback) => {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    methods: ['GET', 'POST', 'OPTIONS'], // Métodos permitidos
-    credentials: true, // Si necesitas enviar cookies o autenticación
-}));
