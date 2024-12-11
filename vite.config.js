ef515ebc38c104905dac16d18f3856bcd399532a
DesideApp <deside.w3app@gmail.com>
2024-12-11 21:49:07 +0100

    axios

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['axios'], // Trata axios como una dependencia externa
+    plugins: [react()],
+    resolve: {
+        alias: {
+            axios: require.resolve('axios'),
+        },
     },
-  },
 });
