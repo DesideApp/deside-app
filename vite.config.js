0b59a283a0db70394d4e2d98090526ab03e81f25
DesideApp <deside.w3app@gmail.com>
2024-12-11 20:13:56 +0100

    Fix axios rollup issue in Vite

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['axios'], // Trata axios como una dependencia externa
+    },
+  },
 });
