76248ff423aee6ab39dda092fded2c2eb9c1c981
DesideApp <deside.w3app@gmail.com>
2024-12-13 01:46:56 +0100

    quitamos uri y ponemos authentication

--- src/components/BottomBar.jsx
+++ src/components/BottomBar.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import "./BottomBar.css";
 import NetworkStatus from './NetworkStatus';
-import { fetchWithAuth } from '../../services/authService.js'; // Importamos fetchWithAuth
+import { fetchWithAuth } from '../../services/authServices.js'; // Importamos fetchWithAuth
 
 function BottomBar() {
     const [solPrice, setSolPrice] = useState(null);
