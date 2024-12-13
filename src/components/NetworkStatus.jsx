1b5ed2269b76044bede1563d2389ad6e295b327e
DesideApp <deside.w3app@gmail.com>
2024-12-13 01:50:31 +0100

    quitamos uri y ponemos authentication

--- src/components/NetworkStatus.jsx
+++ src/components/NetworkStatus.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './NetworkStatus.css';
-import { fetchWithAuth } from '../services/authService'; // Importamos fetchWithAuth
+import { fetchWithAuth } from '../services/authServices'; // Importamos fetchWithAuth
 
 function NetworkStatus({ className }) {
     const [networkStatus, setNetworkStatus] = useState('');
