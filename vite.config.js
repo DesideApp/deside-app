c8fae438cfd85d99ba4ffcfe49b747c638ae8aee
DesideApp <deside.w3app@gmail.com>
2024-12-13 16:26:33 +0100

    quitamos uri y ponemos authentication

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,21 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            stream: 'stream-browserify',
-            url: 'url',
-            buffer: 'buffer',
-        },
+  plugins: [react()],
+  build: {
+    outDir: 'dist', // Asegúrate de que el directorio de salida sea 'dist'
+  },
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      stream: 'stream-browserify',
+      url: 'url',
+      buffer: 'buffer',
     },
-    define: {
-        'process.env': {},
-    },
+  },
+  define: {
+    'process.env': {},
+  },
 });
