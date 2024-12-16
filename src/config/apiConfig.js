16cbe59b2428c3ac04540ff826ad6d6fb510710e
DesideApp <deside.w3app@gmail.com>
2024-12-16 16:25:35 +0100

    Verificar y corregir uso de REACT_APP_BACKEND_URL

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,6 +1,3 @@
-const LOCAL_API_URL = 'https://backend-deside.onrender.com/api'; // URL del backend local
-const PRODUCTION_API_URL = 'https://backend-deside.onrender.com/api'; // URL del backend en producción
-
 const API_BASE_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:3001';
 
 if (!API_BASE_URL) {
