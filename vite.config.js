2cdc4b8e7cbccfbe47799ad976ab134706c232dd
DesideApp <deside.w3app@gmail.com>
2024-12-18 16:36:22 +0100

    issues

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  root: 'public', // Asegúrate de que la raíz esté configurada correctamente
   build: {
-    outDir: 'dist', // Asegúrate de que el directorio de salida sea 'dist'
+    outDir: '../dist', // Asegúrate de que el directorio de salida sea 'dist'
     rollupOptions: {
       external: ['tweetnacl']
     }
