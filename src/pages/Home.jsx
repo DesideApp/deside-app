508ad5fa74ef22e75e55414bdc49ad17aaa4cab4
DesideApp <deside.w3app@gmail.com>
2024-12-20 17:08:19 +0100

    puta madre ya

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,9 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useNavigate } from 'react-router-dom';
 import "./Home.css"; // Importar los estilos específicos
 
 function Home() {
     const navigate = useNavigate();
-
-    useEffect(() => {
-        // Crear un elemento <link> para Google Fonts
-        const link = document.createElement("link");
-        link.href = "https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Montserrat:wght@400;700&family=IBM+Plex+Mono:wght@400;700&display=swap";
-        link.rel = "stylesheet";
-
-        // Añadirlo al <head> del documento
-        document.head.appendChild(link);
-
-        // Limpiar el enlace cuando el componente se desmonte (opcional)
-        return () => {
-            document.head.removeChild(link);
-        };
-    }, []); // Se ejecuta solo una vez al montar
 
     return (
         <div className="home-container">
