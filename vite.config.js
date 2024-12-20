0617e0fa5b23223a1b21579a7065dc8b9bf0cde5
DesideApp <deside.w3app@gmail.com>
2024-12-20 18:14:21 +0100

    puta madre ya

--- vite.config.js
+++ vite.config.js
@@ -2,22 +2,34 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
+const backendUrl = process.env.REACT_APP_BACKEND_URL || 'https://backend-deside.onrender.com';
+
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
       output: {
         manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'], // Mueve las dependencias principales a un archivo separado
-          solana: ['@solana/web3.js'], // Crea un chunk separado para Solana si usas esta librería
+          solana: ['@solana/web3.js'], // Crea un chunk separado para Solana
         },
       },
-    }
+    },
+    chunkSizeWarningLimit: 1000, // Aumentar el límite de tamaño de los chunks a 1000 KiB
   },
   base: '/', // Esto asegura que las rutas sean absolutas
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
+    },
+  },
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: backendUrl, // URL del backend
+        changeOrigin: true,
+        secure: false, // Cambiar a true en producción si usas HTTPS
+      },
     },
   },
 });
