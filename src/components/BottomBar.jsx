5c4905a631c8c89976c2a0eefa778b1b6abfb094
DesideApp <deside.w3app@gmail.com>
2024-12-20 20:47:05 +0100

    puta madre ya

--- src/components/BottomBar.jsx
+++ src/components/BottomBar.jsx
@@ -8,7 +8,6 @@ function BottomBar() {
     useEffect(() => {
         let isMounted = true;
 
-        // Función para obtener el precio de Solana desde el backend
         const fetchSolPrice = async () => {
             try {
                 const response = await fetch('https://backend-deside.onrender.com/api/solana-price');
@@ -17,23 +16,19 @@ function BottomBar() {
                 }
                 const data = await response.json();
                 if (isMounted) {
-                    setSolPrice(data.price); // Ajusta esto según la estructura de la respuesta de tu backend
+                    setSolPrice(data.price);
                 }
             } catch (error) {
                 console.error('Error al obtener el precio de Solana:', error);
                 if (isMounted) {
-                    setSolPrice('N/A'); // Si hay un error, mostrar 'N/A'
+                    setSolPrice('N/A');
                 }
             }
         };
 
-        // Llamada inicial para obtener el precio de Solana
         fetchSolPrice();
-
-        // Intervalo para actualizar el precio cada 10 segundos
         const interval = setInterval(fetchSolPrice, 10000);
 
-        // Limpiar el intervalo al desmontar el componente
         return () => {
             clearInterval(interval);
             isMounted = false;
@@ -42,13 +37,11 @@ function BottomBar() {
 
     return (
         <footer className="bottom-bar">
-            {/* Botones de funciones */}
             <button className="bottom-bar-button">LIVE</button>
             <button className="bottom-bar-button">Lite</button>
             <button className="bottom-bar-button">Pro</button>
             <button className="bottom-bar-button">Ajustes</button>
 
-            {/* Estado de la red y precio de Solana */}
             <div className="network-info">
                 <NetworkStatus className="network-status" />
                 <div className="solana-price">
