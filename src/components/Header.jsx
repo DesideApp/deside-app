d437b341e290e25731d00eddbc0d01bf0f6d6617
DesideApp <deside.w3app@gmail.com>
2025-02-05 15:59:09 +0100

    dividiendo y reajustando las antiguas funciones solanahelpers

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from 'react-router-dom';
-import WalletButton from "./WalletButton"; // Asegurarse de que esta importación sea correcta
+import WalletButton from "./WalletButton.jsx"; // Asegurarse de que esta importación sea correcta
 import "./Header.css";
 
 function Header() {
