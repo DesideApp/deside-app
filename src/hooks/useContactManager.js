9fdb4e3e128e7844542c789f26f70793c47540c5
DesideApp <deside.w3app@gmail.com>
2025-02-14 19:49:13 +0100

    fixing import

--- src/hooks/useContactManager.js
+++ src/hooks/useContactManager.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
-import { getConnectedWallet } from "../../services/walletService";
-import { fetchWithAuth } from "../../services/authServices";
+import { getConnectedWallet } from "../services/walletService";
+import { fetchWithAuth } from "../services/authServices";
 
 export default function useContactManager() {
     const [confirmedContacts, setConfirmedContacts] = useState([]);
