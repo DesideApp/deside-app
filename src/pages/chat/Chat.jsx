26d796004f946e1c59fd520d7eb49f2512a3bf62
DesideApp <deside.w3app@gmail.com>
2025-02-17 13:52:57 +0100

    remove chat getconnect

--- src/pages/chat/Chat.jsx
+++ src/pages/chat/Chat.jsx
@@ -1,30 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import ContactList from "../../components/chatcomps/ContactList.jsx";
 import ChatWindow from "../../components/chatcomps/ChatWindow.jsx";
 import RightPanel from "../../components/chatcomps/RightPanel.jsx";
 import WalletModal from "../../components/common/WalletModal.jsx"; 
-import { getConnectedWallet } from "../../services/walletService.js";
 import "./Chat.css";
 
 function Chat() {
-    const [walletAddress, setWalletAddress] = useState(null);
     const [selectedContact, setSelectedContact] = useState(null);
     const [isModalOpen, setIsModalOpen] = useState(false);
-
-    useEffect(() => {
-        const updateWalletStatus = async () => {
-            const connectedWallet = await getConnectedWallet();
-            setWalletAddress(connectedWallet?.walletAddress || null);
-        };
-
-        updateWalletStatus();
-        window.addEventListener("walletConnected", (e) => {
-            setWalletAddress(e.detail.wallet);
-            setIsModalOpen(false); 
-        });
-
-        return () => window.removeEventListener("walletConnected", updateWalletStatus);
-    }, []);
 
     return (
         <div className="chat-page-container">
