ce750e67956849f73601977ca96000b6ea311b4c
DesideApp <deside.w3app@gmail.com>
2025-03-05 04:31:51 +0100

    varias correcciones

--- src/components/layout/BottomBar.jsx
+++ src/components/layout/BottomBar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./BottomBar.css";
 import NetworkStatus from "./NetworkStatus.jsx";
-import SolanaPrice from "../SolanaPrice.jsx";
+import SolanaPrice from "./SolanaPrice.jsx";
 
 const BottomBar = React.memo(() => {
     return (
