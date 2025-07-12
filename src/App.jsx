3e32c88229dc9e72dbb745ce533e2752248b3f16
DesideApp <deside.w3app@gmail.com>
2025-07-12 02:00:22 +0200

    Tu mensaje de commit aquí

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ServerProvider } from "./contexts/ServerContext.jsx";
+import { LayoutProvider } from "./contexts/LayoutContext.jsx";
 import Layout from "./Layout.jsx";
-import NotificationContainer from "./components/common/NotificationContainer.jsx"; // ✅ Importación añadida
+import NotificationContainer from "./components/common/NotificationContainer.jsx";
 
 function App() {
     return (
         <ServerProvider>
-            <Router>
-                <Layout />
-                <NotificationContainer />
-            </Router>
+            <LayoutProvider>
+                <Router>
+                    <Layout />
+                    <NotificationContainer />
+                </Router>
+            </LayoutProvider>
         </ServerProvider>
     );
 }
 
-export default App
+export default App;
